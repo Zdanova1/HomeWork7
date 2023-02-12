a195eef810bafcfe38f19268c4e9dcc2a8197faf
Олеся <lisenok56@yandex.ru>
2023-02-12 17:15:18 +0300

    ProgAdd

---
+++ RowMinSumElements/Program.cs
@@ -0,0 +1,59 @@
+﻿// В прямоугольной матрице найти строку с наименьшей суммой элементов.
+
+int[,] GetArray(int row, int col, int min, int max)
+{
+    int[,] array = new int[row, col];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(min, max);
+        }
+    }
+    return array;
+}
+int rowNumber(int[,] array)
+{
+    int index = 0;
+    int sum = 0;
+    int result = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        sum = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum += array[i, j];
+        }
+        if (i == 0) result = sum;
+        else if (sum < result)
+        {
+            result = sum;
+            index = i;
+        }
+    }
+    return index;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write(array[i, j] + "  ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int row = 5;
+int col = 5;
+int min = 1;
+int max = 10;
+
+int[,] array = GetArray(row, col, min, max);
+PrintArray(array);
+System.Console.WriteLine("---");
+int index = rowNumber(array);
+System.Console.WriteLine(index);
+
